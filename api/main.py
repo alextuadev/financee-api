67bfd347afe0df68450b872a863d5dcffde9cbfc
Alexis Borges <tuaalexis@gmail.com>
2023-04-19 06:54:32 -0600

    Added new debt types routers

--- api/main.py
+++ api/main.py
@@ -7,16 +7,18 @@ from routers.auth import auth_router
 from routers.bank_account import bank_account_router
 from routers.credit_card import credit_card_router
 from routers.debt import debt_router
+from routers.debt_type import debt_type_router
 
 
 app = FastAPI()
-app.version = "0.0.5"
+app.version = "0.0.6"
 app.title = "Financee API"
 app.add_middleware(ErrorHandler)
 app.include_router(auth_router, prefix="/api")
 app.include_router(bank_router, prefix="/api")
 app.include_router(bank_account_router, prefix="/api")
 app.include_router(credit_card_router, prefix="/api")
+app.include_router(debt_type_router, prefix="/api")
 app.include_router(debt_router, prefix="/api")
 # app.include_router(transaction_router, prefix="/api")
 
